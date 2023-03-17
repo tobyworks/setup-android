1b63a66a7ff2114a48345a821ed3d64e9b0b8c68
2bworks <t.bonhof@gmail.com>
2023-03-17 17:57:25 +0100

    commit

--- dist/index.js
+++ dist/index.js
@@ -9801,7 +9801,11 @@ function run() {
         const sdkManager = yield installSdkManager();
         core.debug(`sdkmanager installed to: ${sdkManager}`);
         yield callSdkManager(sdkManager, '--licenses');
-        yield callSdkManager(sdkManager, 'tools');
+        yield callSdkManager(sdkManager, '--licenses');
+        yield callSdkManager(sdkManager, '--licenses');
+        yield callSdkManager(sdkManager, '--licenses');
+        yield callSdkManager(sdkManager, '--licenses');
+        yield callSdkManager(sdkManager, '--licenses');
         yield callSdkManager(sdkManager, 'platform-tools');
         core.setOutput('ANDROID_COMMANDLINE_TOOLS_VERSION', COMMANDLINE_TOOLS_VERSION);
         core.exportVariable('ANDROID_HOME', ANDROID_SDK_ROOT);
